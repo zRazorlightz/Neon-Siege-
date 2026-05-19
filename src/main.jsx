e7be5d7eb3e1313f156e787ed082c673fc788eef
zRazorlightz <61505698+zRazorlightz@users.noreply.github.com>
2026-05-19 19:26:23 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NeonShooter from './NeonShooter.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <NeonShooter />
-)
