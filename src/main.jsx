00309d6f0702c650e0de0edeb6566a4238d842e6
zRazorlightz <61505698+zRazorlightz@users.noreply.github.com>
2026-05-19 19:21:52 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NeonShooter from './NeonShooter.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <NeonShooter />
+)
